43b2b376cfbfe20f1893781b9df56fbe35f2279f
Torbjörn Sjögren <torbjorn.sjogren@postnord.com>
2024-04-08 12:12:43 +0200

    load

---
+++ ecs/load.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+auth="elastic:elastic"
+
+version="$(cat version)"
+for file in `ls generated/elasticsearch/composable/component/*.json`
+do
+  fieldset=`echo $file | cut -d/ -f5 | cut -d. -f1 | tr A-Z a-z`
+  component_name="ecs_${version}_${fieldset}"
+  api="_component_template/${component_name}"
+
+  # echo "$file => $api"
+  curl -k --user "$auth" -XPUT "https://localhost:9200/$api" --header "Content-Type: application/json" -d @"$file"
+done
